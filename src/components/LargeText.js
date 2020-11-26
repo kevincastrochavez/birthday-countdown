471aad8088a8a3e734deb797ce69b5ea3fe4627c
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-26 10:51:30 -0600

    finished last styles

---
+++ src/components/LargeText.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LargeText({ text }) {
+  return <div className="largeText">{text}</div>;
+}
+
+export default LargeText;
--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Button from "./Button";
 import ChangeDate from "./ChangeDate";
 import Clock from "./Clock";
+import LargeText from "./LargeText";
 import Picker from "./Picker";
 
 export default class App extends Component {
@@ -21,14 +22,19 @@ export default class App extends Component {
           title="Change Date"
           callback={() => this.setState({ active: false })}
         />,
+        <LargeText text="04/03" />,
+        <label className="grid__remaining">
+          Remaining until your 21st birthday
+        </label>,
       ];
     } else {
-      return (
+      return [
+        <Picker />,
         <Button
           title="Generate Countdown"
           callback={() => this.setState({ active: true })}
-        />
-      );
+        />,
+      ];
     }
   }.bind(this);
 
@@ -43,8 +49,6 @@ export default class App extends Component {
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
-
-        <Picker />
 
         {this.renderItems()}
       </div>
